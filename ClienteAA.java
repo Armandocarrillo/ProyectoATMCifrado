8b2015e8162cf3f5398870b5928ca3ef404993e4
Armando Carrillo <ingcompuarmando@gmail.com>
2021-02-09 19:24:36 -0600

    Suma en depositar

--- ClienteAA.java
+++ ClienteAA.java
@@ -4,39 +4,44 @@
  import java.io.*;
  import java.security.*;
  import javax.crypto.*;
-
-
- public class ClienteAA
+import java.util.Scanner;
+/*se ingresaron las librerias*/
+ public class ClienteAA /*Se crea la clase ClienteAA*/
  {
- public static void main(String a[]) throws Exception
+ public static void main(String a[]) throws Exception/*creamos la funcion principal*/
  {
-  Socket socket = null;
-  String peticion = null;
-  String respuesta = null;
+ 	BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
+ //	Scanner sc = new Scanner(System.in);  
+  Socket socket = null;/*Creamos un socket */
+  String peticion = null;/*variable string*/
+  String respuesta = null;/*variable de tipo cadena*/
 
   byte arreglo[] = new byte[10000];
 
- if (a.length > 0){
- 	String valorPuerto = a[1];
+ if (a.length > 0){/*Creamos un if para verificar que se hayan pasado los 
+ 	argumentos en ClienteAA si cumple con la condicion que se mayor 
+ 	realiza lo siguiente*/
+ 	String valorPuerto = a[1];/*Si este cumple se le asigna a valorPuerto el valor de a[1]*/
 
  	try
  	{// inicio try 1
-
+ 		/*Creamos un nuevo objeto y que guardara en un nuevo archivo llamado llave.ser*/
  	 ObjectInput in = new ObjectInputStream(new FileInputStream("llave.ser"));
  	 Key llave = (Key)in.readObject();
  	 System.out.println( "llave=" + llave );
  	 in.close();
  	 System.out.println("Me conecto al puerto del servidor "+ valorPuerto);
-  	 int cambioStringAInt = Integer.valueOf(valorPuerto);
+ 	 /*se imprime en pantalla*/
+  	 int cambioStringAInt = Integer.valueOf(valorPuerto);/*Se crea una variable cambioStringAInt
+  	 se le cambia el valor por lo que contiene la var valorPuerto*/
 
   	socket = new Socket(a[0],cambioStringAInt);
  	 DataInputStream dis = new DataInputStream( socket.getInputStream() );
  	 DataOutputStream dos = new DataOutputStream( socket.getOutputStream() );
 
-
-
+int tDepo=0;
+/*Se aplica la seccion de la llave para poder leer  */
  	 while(true){ //inicio while 2
-
 
  			int bytesLeidos = dis.read(arreglo);
  			byte arreglo2[]	= new byte[bytesLeidos];
@@ -52,52 +57,129 @@
  			System.out.println( "\n\nMensaje recibido del servidor :" );
  			String plainTextReceived = new String(newPlainText, "UTF8");
  			System.out.println( new String(newPlainText, "UTF8") );
+ 			/*aqui Se aplica la comparacion de palabra y la accion para el servidor */
  			if (plainTextReceived.equals("SALIR")){
  				System.out.println("\n\n Cerrando Conexión");
  		 		break;
  			 }
  			System.out.println( "\n\n Escribe un mensaje para el servidor:" );
+			/*impresion en pantalla */
 
-
- 			BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
+ 			
  			peticion = br.readLine();
  			 System.out.println( "Ahora encriptamos el mensaje para el servidor..." );
- 			 byte[] arrayPeticion = peticion.getBytes();
+ 			 byte[] arrayPeticion = peticion.getBytes();/*codigo de envio*/
  			 Cipher cifrar = Cipher.getInstance("DES");
  			 cifrar.init(Cipher.ENCRYPT_MODE, llave);
  			 byte[] cipherText = cifrar.doFinal( arrayPeticion );
  			 bytesToBits( cipherText );
- 			 dos.write( cipherText, 0, cipherText.length );
- 			 if (peticion.equals("SALIR")){
+ 			 dos.write( cipherText, 0, cipherText.length );/*codigo de envio*/
+ 			 /*en caso dado se aplican comparaciones de lo que hara el usuario*/
+ 			 if (peticion.equals("SALIR"))
+ 			 {
  			 	System.out.println("\n\nCerrando conexion con el servidor");
  		 		break;
+
+ 			 }
+ 			  
+ 			 if (peticion.equals("DEPOSITAR")) 
+ 			 {
+ 			 System.out.println("Cuanto es lo que deseas depositar");
+ 			///////////////////////////////////////////////////////////////////////
+ 			 String depositado = br.readLine();
+ 			
+ 			 byte[] arrayDepositado = depositado.getBytes();/*codigo de envio*/
+ 			 
+ 			 cifrar.init(Cipher.ENCRYPT_MODE, llave);
+ 			 byte[] cipherDepositado = cifrar.doFinal( arrayDepositado );
+ 			 bytesToBits( cipherDepositado );
+ 			 dos.write( cipherDepositado, 0, cipherDepositado.length );/*codigo de envio*/
+ 			 //int deposit=50;
+ 			//enviar al sertvi
+
+ 			  //System.out.println("Tienes : "+ tDepo);
+ 			 /// tDepo = (in);
+ 			 //  tDepo = tDepo + depositado;
+ 			
+ 			  // System.out.println("Tienes : "+ tDepo);
+			///////////////////////////////////////////////////////////////////////
  			 }
 
+ 			  if (peticion.equals("CONSULTAR")) 
+ 			 {
+ 			 System.out.println("\n\tTu dinero total es:");
+ 			 ///////////////////////////////////////////////////////////////////////
+ 			 String consultar = br.readLine();
+ 			
+ 			 byte[] arrayConsultar = consultar.getBytes();/*codigo de envio*/
+ 			 
+ 			 cifrar.init(Cipher.ENCRYPT_MODE, llave);
+ 			 byte[] cipherConsultar = cifrar.doFinal( arrayConsultar );
+ 			 bytesToBits( cipherConsultar );
+ 			 dos.write( cipherConsultar, 0, cipherConsultar.length );/*codigo de envio*/
+ 			
+ 			//enviar al sertvi
+
+ 			 // System.out.println("Tienes : "+ tDepo);
+ 			 /// tDepo = (in);
+ 			 //  tDepo = tDepo + depositado;
+ 			
+ 			   //System.out.println("Tienes : "+ tDepo);
+ 			 ///////////////////////////////////////////////////////////////////////  
+ 			 }
+
+ 		
+ 			  if (peticion.equals("RETIRAR")) 
+ 			 {
+ 			 System.out.println("\n\tEXTRAYENDO SU DINERO... ... ...");
+ 			 //int guardado=0;
+
+			 ///////////////////////////////////////////////////////////////////////
+ 			 String retirado = br.readLine();
+ 			
+ 			 byte[] arrayRetirado = retirado.getBytes();/*codigo de envio*/
+ 			 
+ 			 cifrar.init(Cipher.ENCRYPT_MODE, llave);
+ 			 byte[] cipherRetirado = cifrar.doFinal( arrayRetirado );
+ 			 bytesToBits( cipherRetirado );
+ 			 dos.write( cipherRetirado, 0, cipherRetirado.length );/*codigo de envio*/
+ 			 //int deposit=50;
+ 			//enviar al sertvi
+
+ 			  //System.out.println("Tenias : "+ tDepo);
+ 			
+ 			//codigo de regreso del servidor::
+
+
+ 			 // System.out.println("Tienes : "+ tDepo);
+ 			 ///////////////////////////////////////////////////////////////////////  
+ 			 
+ 			 // System.out.println("\n\tSU DINERO TOTAL ES  : "+ guardado);
+ 			   
+ 			 }
+ 			 /*tenemos varios ciclos para poder efectuar en la pantalla del cliente 
+ 			 todps estos son comparaciones con las palabras clave*/
  		} //cierre while 2
 
-
- 	 dos.close();
- 	 dis.close();
- 	 socket.close();
+ 	 dos.close();/*Cerramos variable escribir*/
+ 	 dis.close();/*Cerramos variable de lectura*/
+ 	 socket.close();/*Cerramos el socket fin*/
  	}
- 	catch(IOException e)
+ 	catch(IOException e)/*en caso de exista la excepcion capturamos en "e" el valor 
+ 	para que este se ejecute normalmente*/
  	{
 
  	 System.out.println("java.io.IOException generada");
- 	 e.printStackTrace();
+ 	 e.printStackTrace();/*impreision en pantalla del IOException*/
  	}
 
-
-
-
-
  } else {//cierre if puerto
- 	System.out.println("No ha pasado el puerto");
+ 	System.out.println("No ha pasado el puerto");/*impresion en pantalla*/
 
  } //cierre else
 
  } //cierre main
-
+/*lectura de los bits paso apaso */
  public static void bytesToBits( byte[] texto )
  	{ // inicio bytes
  		StringBuilder stringToBits = new StringBuilder();
@@ -115,6 +197,7 @@
  			stringToBits.append( binary );
  		}
  		System.out.println( "El mensaje completo en bits es:" + stringToBits );
+ 		/*muestra el mensaje completo*/
  	} //cierre bytes
 
  }// cierre class 
--- ServidorAA.java
+++ ServidorAA.java
@@ -4,34 +4,37 @@ import java.io.*;
 import java.security.*;
 import javax.crypto.*;
 import java.util.Scanner;
-
-public class ServidorAA
+//se ingresaron las librerias que usaremos
+public class ServidorAA /*Se crea la clase ServidorAA*/
 {
-public static void main(String a[]) throws Exception
-{ 
+public static void main(String a[]) throws Exception/*Iniciamos nuestra Funcion principal*/
+{ /*Creamos nuestras variables, creamos el socker para el servidor*/
  ServerSocket serverSocket = null;
  Socket socket = null;
  String peticion = null;
  String peticionInicial = null;
  String respuesta = null;
  Integer contador = 0;
+ Integer saldo = 0;
+ Integer suma = 0;
+ Integer cambioArreglodepositadoAInt = 0;
 String bienvenida = "Bievenido a ATM\n\tPresiona una opcion:\nCONSULTAR\nDEPOSITAR\nRETIRAR\n";
-
-
 
 if (a.length > 0){ //inicio if 1
 	String valorPuerto = a[0];
-
+/*Se genera la sweccion de nuestra llave */
 File f = new File("llave.ser");
 	if(!f.exists()) { 
 	
-		System.out.println( "Generando la llave..." );
+		System.out.println( "Generando la llave..." );/*Impresion en pantalla de llave*/
 		KeyGenerator keyGen = KeyGenerator.getInstance("DES");
 		keyGen.init(56);
-		Key newKey = keyGen.generateKey();
-		System.out.println( "llave=" + newKey );
+		Key newKey = keyGen.generateKey();/*Se le asigna a new key el valor de la llave generada*/
+		System.out.println( "llave=" + newKey );/*Muestra nuestra llave*/
 		System.out.println( "Llave generada!" );
 
+		/*En los puntos siguientes se genera el archivo donde se guardara nuestra llave
+		y este pueda ser compartido con el cliente*/
 		ObjectOutput out = new ObjectOutputStream(new FileOutputStream("llave.ser"));
 		out.writeObject( newKey );
 		out.close();
@@ -44,32 +47,32 @@ File f = new File("llave.ser");
 
 
 
-	try
+	try/*asinamos la seccion del puerto en espera*/
 	{// inicio try 1
 	 System.out.println("Escuchando por el puerto" + valorPuerto);
  	int cambioStringAInt = Integer.valueOf(valorPuerto);
 	serverSocket = new ServerSocket(cambioStringAInt);
 	}// fin try 1
 	catch(IOException e)
-	{
-	 System.out.println("java.io.IOException generada");
+	{/*Inicio de CATCH*/
+	 System.out.println("java.io.IOException generada");/*Impresion en pantalla de la excepcion*/
 	 e.printStackTrace();
-	}
+	}/*Final de CATCH*/
 
-	System.out.println("Esperando a que los clientes se conecten...");
+	System.out.println("Esperando a que los clientes se conecten...");/*impresion en pantalla*/
 while(true)
-	{ //inicio  while 1
+	{ //inicio  while ciclo
 	 try
 	 { //inicio 2 try
-		socket = serverSocket.accept();
+		socket = serverSocket.accept();/*Se acepta el socket creado se le asigna el valor a socket*/
 		System.out.println("Se conecto un cliente: " + socket.getInetAddress().getHostName());
 		DataInputStream dis = new DataInputStream( socket.getInputStream() );
 		DataOutputStream dos = new DataOutputStream( socket.getOutputStream() );
 		
-		///
+		/*desplegamos en pantalla el menu guardado en bienvenida*/
 		peticionInicial = bienvenida;
 		 System.out.println( "Mi peticion es: " + peticionInicial );
-		 System.out.println( "Ahora encriptamos la peticion..." );
+		 System.out.println( "Ahora encriptamos la peticion..." );/*Impreison en pantalla*/
 		 byte[] arrayPeticionInicial = peticionInicial.getBytes();
 		 Cipher cifrarInicial = Cipher.getInstance("DES");
 		 cifrarInicial.init(Cipher.ENCRYPT_MODE, llave);
@@ -78,7 +81,7 @@ while(true)
 		 
 		 dos.write( cipherTextInicial, 0, cipherTextInicial.length );
 		
-
+		 /*Seccion de encriptar el mensaje*/
 
 		 while(true){//inicio while 2
 
@@ -96,7 +99,7 @@ while(true)
 		 
 		 dos.write( cipherText, 0, cipherText.length );
 		 
-		 if (peticion.equals("SALIR")){
+		 if (peticion.equals("SALIR")){/*se efectua si el cliente ingresa SALIR */
 		 	break;
 		 }
 		 
@@ -106,7 +109,7 @@ while(true)
 		for(int i=0; i < bytesLeidos; i++ ){
 			arreglo2[i] = arreglo[i];
 		}
-		
+		/*Seccion de encriptado*/
 		Cipher decifrar = Cipher.getInstance("DES");
 		decifrar.init(Cipher.DECRYPT_MODE, llave);
 		bytesToBits( arreglo2 );
@@ -115,32 +118,74 @@ while(true)
 		String messageReceivedInPlainText = new String(newPlainText, "UTF8") ;
 		System.out.println( new String(newPlainText, "UTF8"));
 		
+		/*Ciclo if en caso de que el cliente ejecute una accion en el servidor nos
+		mostrara las siguientes lienas*/
 		if (messageReceivedInPlainText.equals("SALIR")){
 			System.out.println("\n\n El servidor ha cerrado la conexion");
 		 	break;
 		} else if (messageReceivedInPlainText.equals("DEPOSITAR")){
 			System.out.println("\n\n El cliente esta depositando");
 
+			bytesLeidos = dis.read(arreglo);
+
+			byte arregloDepositado[]	= new byte[bytesLeidos];
+		for(int i=0; i < bytesLeidos; i++ ){
+			arregloDepositado[i] = arreglo[i];
+		}
+		/*Seccion de encriptado*/
+		decifrar.init(Cipher.DECRYPT_MODE, llave);
+		bytesToBits( arregloDepositado );
+		byte[] newDeposit = decifrar.doFinal(arregloDepositado);
+		System.out.println( "\n\nMensaje recibido del cliente :" );
+		String messageReceivedInDeposit = new String(newDeposit, "UTF8") ;
+		System.out.println( new String(newDeposit, "UTF8"));
+		//
+		//  arregloDepositado=Integer.paser   Recordar era para convertir
+		cambioArreglodepositadoAInt = Integer.parseInt(messageReceivedInDeposit);
+
+
+		suma = cambioArreglodepositadoAInt + suma;
+		//arregloDepositado = Integer.parseInt();
+		System.out.println( "\t Tu nuevo saldo es:" + suma );
+
 		 	continue;
 		} else if (messageReceivedInPlainText.equals("CONSULTAR")){
 			System.out.println("\n\n El cliente esta consultando");
+		 ///////////////////////////////////
+		 	bytesLeidos = dis.read(arreglo);
+
+			byte arregloConsultado[]	= new byte[bytesLeidos];
+		for(int i=0; i < bytesLeidos; i++ ){
+			arregloConsultado[i] = arreglo[i];
+		}
+		/*Seccion de encriptado*/
+		decifrar.init(Cipher.DECRYPT_MODE, llave);
+		bytesToBits( arregloConsultado );
+		byte[] newConsult = decifrar.doFinal(arregloConsultado);
+		System.out.println( "\n\nMensaje recibido del cliente :" );
+		String messageReceivedInDeposit = new String(newConsult, "UTF8") ;
+		System.out.println( new String(newConsult, "UTF8"));
+		System.out.println( "\t Tu nuevo saldo es:" + suma );
+
+		//
+		//   arregloConsultado=Integer.paser
+//////////////////////////////////////////////////////////////////////
+
 		 	continue;
 		} else if (messageReceivedInPlainText.equals("RETIRAR")){
 			System.out.println("\n\n El cliente esta retirando");
 		 	continue;
-		}
-
-		 	
+		} 	
 	 	
 	 }// fin while 2
 
-		dos.close();
-		dis.close();
-		socket.close();
+		dos.close();/*Se cierra el modo de escritura*/
+		dis.close();/*Se cierra el modo de lectura */
+		socket.close();/*Cierre del socket se asigna null*/
 	 } //fin 2 try
 	 catch(IOException e)
 	 {
-		System.out.println("java.io.IOException generada");
+		System.out.println("java.io.IOException generada");/*Impresio de excepcion*/
 		e.printStackTrace();
 	 }
 	}// fin while 1
@@ -149,7 +194,7 @@ while(true)
 
 
 } else {  // cierre del if 1 e inicio del else 1
-	System.out.println("No ha pasado el puerto");
+	System.out.println("No ha pasado el puerto");/*si es que no pasa el puerto*/
 } // cierre else 1
 
 
@@ -159,7 +204,7 @@ public static void bytesToBits( byte[] texto )
 	{ // static bytesToBits
 		StringBuilder stringToBits = new StringBuilder();
 		for( int i=0; i < texto.length; i++ )
-		{
+		{/*aplicacion del for */
 			StringBuilder binary = new StringBuilder();
 			byte b = texto[i];
 			int val = b;
@@ -171,7 +216,7 @@ public static void bytesToBits( byte[] texto )
 			System.out.println( (char)b + " \t " + b + " \t " + binary );
 			stringToBits.append( binary );
 		}
-		System.out.println( "El mensaje completo en bits es:" + stringToBits );
+		System.out.println( "El mensaje completo en bits es:" + stringToBits );/*mensaje terminado*/
 	} // Fin static bytesToBits
 
 
